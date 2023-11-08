1675e4cda65f8c8b5456481743052090d043c1c3
2dama <2dama@informatica.edu>
2023-11-08 15:22:39 +0000

    Initial commit

---
+++ app/src/main/java/data/Pregunta.kt
@@ -0,0 +1,6 @@
+package data
+
+import android.media.Image
+import androidx.compose.ui.graphics.painter.Painter
+
+data class Pregunta(var pregunta : String, var resultado : Boolean, var imagen : Int?)
---
+++ app/src/main/java/ui/MainScreen.kt
@@ -0,0 +1,223 @@
+package ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.appbuzz.R
+import com.example.appbuzz.ui.theme.AppBuzzTheme
+import data.Pregunta
+
+class MainActivity : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppBuzzTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MainScreen() {
+
+    /*TODO =
+        *  -> Cambiar forma para añadir las preguntas*/
+
+    val listaPreguntas: ArrayList<Pregunta> = ArrayList()
+
+    val p1: Pregunta = Pregunta(
+        "El Club Deportivo Tenerife fue fundado en 1912", true, R.drawable.cdt_escudo
+    )
+    val p2: Pregunta = Pregunta(
+        "El Girona eliminó al club tinerfeño en los playoffs de la temporada 22/23",
+        true,
+        R.drawable.laliga_hyper
+    )
+    val p3: Pregunta = Pregunta(
+        "La UD Las Palmas ha disputado menos partidos en 1ª división que el CD Tenerife",
+        false,
+        R.drawable.cdt_escudo
+    )
+    val p4: Pregunta =
+        Pregunta(
+            "El máximo goleador de la historia del CD Tenerife es Suso Santana", false, R.drawable.laliga_hyper
+        )
+    val p5: Pregunta = Pregunta(
+        "Aun así el CD Tenerife sigue siendo superior a la UD Las Palmas (no acepto discusion)",
+        true,
+        R.drawable.cdt_escudo
+    )
+
+    listaPreguntas.add(p1)
+    listaPreguntas.add(p2)
+    listaPreguntas.add(p3)
+    listaPreguntas.add(p4)
+    listaPreguntas.add(p5)
+
+    var preguntaActual by remember {
+        mutableStateOf(0)
+    }
+
+    var respuestaUsuario: Boolean? by remember {
+        mutableStateOf(null)
+    }
+
+    var imagen: Int? by remember {
+        mutableStateOf(null)
+    }
+
+    val pregunta = if (preguntaActual < listaPreguntas.size) {
+        listaPreguntas[preguntaActual]
+    } else if (preguntaActual > listaPreguntas.size) {
+        listaPreguntas[0]
+    } else {
+        null
+    }
+
+    var textoPregunta by remember {
+        mutableStateOf("Pregunta Default")
+    }
+
+    var textoResultado by remember {
+        mutableStateOf("")
+    }
+
+    if (pregunta != null) {
+        val resultado = pregunta.resultado
+        imagen = pregunta.imagen
+        textoPregunta = pregunta.pregunta
+
+        if (resultado) {
+            textoResultado.equals("Respuesta Correcta")
+        } else {
+            textoResultado.equals("Respuesta Incorrecta")
+        }
+
+        Column(
+            verticalArrangement = Arrangement.SpaceEvenly, modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+        ) {
+
+            /*TODO =
+            *  -> Seleccionar pregunta segun clase "Pregunta"
+            *  [!] El texto actual es temporal, solo para ordenar los elementos
+            * */
+
+            Text(
+                text = textoPregunta,
+                modifier = Modifier.fillMaxWidth(),
+                fontSize = 25.sp,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.SemiBold
+            )
+
+            /*TODO =
+            *  -> Seleccionar imagen segun clase "Pregunta"
+            *  -> Cambiar en conjunto a la pregunta
+            *  [!] La imagen actual es temporal, solo para ordenar los elementos
+            * */
+
+            pregunta.imagen?.let { painterResource(id = it) }?.let {
+                Image(
+                    painter = it,
+                    contentDescription = "",
+                    Modifier
+                        .fillMaxWidth()
+                        .align(CenterHorizontally)
+                        .size(250.dp)
+                )
+            }
+
+            if (respuestaUsuario != null) {
+                textoResultado = if (respuestaUsuario == resultado) {
+                    "Respuesta correcta"
+                } else {
+                    "Respuesta incorrecta"
+                }
+            } else {
+                textoResultado = ""
+            }
+
+            /*TODO =
+            *  -> Seleccionar el texto segun la respuesta del usuario y la respuesta real
+            *  -> El texto aparecera invisible hasta que se seleccione una de las dos respuestas
+            *  -> Cambiar color del texto segun resultado
+            *  [!] El texto actual es temporal, solo para ordenar los elementos
+            * */
+
+            Text(
+                text = textoResultado,
+                modifier = Modifier.fillMaxWidth(),
+                fontSize = 23.sp,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.SemiBold
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Button(onClick = {
+                    respuestaUsuario = true /*TODO -> Comprobar si es verdadero
+                                      -> Cambiar color segun resultado */
+                }) {
+                    Text(text = "True", fontSize = 16.sp)
+                }
+                Button(onClick = {
+                    respuestaUsuario = false/*TODO -> Comprobar si es falso
+                                      -> Cambiar color segun resultado */
+                }) {
+                    Text(text = "False", fontSize = 16.sp)
+                }
+            }
+
+            Button(
+                onClick = {
+                    preguntaActual++
+                    respuestaUsuario = null
+                    /*TODO -> Pasar a la siguiente pregunta*/
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "SIGUIENTE")
+            }
+        }
+    }
+}
