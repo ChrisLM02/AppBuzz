16fe1ae88a5d92c9a648faec7b65d0761647c01e
chris <christianclm02@gmail.com>
2023-11-11 19:15:24 +0000

    11/11/2023 - Finished

--- app/src/main/java/ui/MainScreen.kt
+++ app/src/main/java/ui/MainScreen.kt
@@ -10,8 +10,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.CompareArrows
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,6 +25,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
@@ -32,6 +39,7 @@ import androidx.compose.ui.unit.sp
 import com.example.appbuzz.R
 import com.example.appbuzz.ui.theme.AppBuzzTheme
 import data.Pregunta
+import java.util.Random
 
 class MainActivity : ComponentActivity() {
 
@@ -67,21 +75,23 @@ fun MainScreen() {
     val p2: Pregunta = Pregunta(
         "El Girona eliminó al club tinerfeño en los playoffs de la temporada 22/23",
         true,
-        R.drawable.laliga_hyper
+        R.drawable.girona_escudo
     )
     val p3: Pregunta = Pregunta(
         "La UD Las Palmas ha disputado menos partidos en 1ª división que el CD Tenerife",
         false,
-        R.drawable.cdt_escudo
+        R.drawable.udlp_escudo
     )
     val p4: Pregunta =
         Pregunta(
-            "El máximo goleador de la historia del CD Tenerife es Suso Santana", false, R.drawable.laliga_hyper
+            "El máximo goleador de la historia del CD Tenerife es Suso Santana",
+            false,
+            R.drawable.suso_imagen
         )
     val p5: Pregunta = Pregunta(
         "Aun así el CD Tenerife sigue siendo superior a la UD Las Palmas (no acepto discusion)",
         true,
-        R.drawable.cdt_escudo
+        R.drawable.cdt_udlp_escudos
     )
 
     listaPreguntas.add(p1)
@@ -117,6 +127,9 @@ fun MainScreen() {
     var textoResultado by remember {
         mutableStateOf("")
     }
+
+    var colorBotonVerdadero by remember { mutableStateOf(Color.White) }
+    var colorBotonFalso by remember { mutableStateOf(Color.White) }
 
     if (pregunta != null) {
         val resultado = pregunta.resultado
@@ -194,30 +207,91 @@ fun MainScreen() {
                 horizontalArrangement = Arrangement.Center,
                 modifier = Modifier.fillMaxWidth()
             ) {
-                Button(onClick = {
-                    respuestaUsuario = true /*TODO -> Comprobar si es verdadero
-                                      -> Cambiar color segun resultado */
-                }) {
-                    Text(text = "True", fontSize = 16.sp)
+                Button(
+                    onClick = {
+                        respuestaUsuario = true
+                        colorBotonVerdadero =
+                            if (respuestaUsuario == pregunta.resultado) Color.Green else Color.Red
+                        colorBotonFalso = Color.Black
+                        /*TODO -> Comprobar si es verdadero
+                                              -> Cambiar color segun resultado */
+                    }
+                ) {
+                    Text(text = "True", color = colorBotonVerdadero, fontSize = 16.sp)
                 }
-                Button(onClick = {
-                    respuestaUsuario = false/*TODO -> Comprobar si es falso
-                                      -> Cambiar color segun resultado */
-                }) {
-                    Text(text = "False", fontSize = 16.sp)
+
+                Button(
+                    onClick = {
+                        respuestaUsuario = false
+                        colorBotonFalso =
+                            if (respuestaUsuario == pregunta.resultado) Color.Green else Color.Red
+                        colorBotonVerdadero = Color.Black
+                        /*TODO -> Comprobar si es falso
+                                              -> Cambiar color segun resultado */
+                    }
+                ) {
+                    Text(text = "False", color = colorBotonFalso, fontSize = 16.sp)
                 }
             }
 
-            Button(
-                onClick = {
-                    preguntaActual++
-                    respuestaUsuario = null
-                    /*TODO -> Pasar a la siguiente pregunta*/
-                },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(text = "SIGUIENTE")
+
+            Row(horizontalArrangement = Arrangement.Center, modifier = Modifier.fillMaxWidth()) {
+                Button(
+                    onClick = {
+                        preguntaActual =
+                            (preguntaActual - 1 + listaPreguntas.size) % listaPreguntas.size
+                        respuestaUsuario = null
+                        /*TODO -> Volver a la pregunta anterior*/
+                        if (preguntaActual == listaPreguntas.size - 1) {
+                            textoPregunta = listaPreguntas.last().pregunta
+                            imagen = listaPreguntas.last().imagen
+                        }
+                        colorBotonVerdadero = Color.Unspecified
+                        colorBotonFalso = Color.Unspecified
+                    }
+
+                ) {
+                    Icon(Icons.Default.ArrowBack, contentDescription = "Icono Flecha Atras")
+                    Text(text = "ANTERIOR")
+                }
+
+                Button(
+                    onClick = {
+                        seleccionarPreguntaAlAzar(listaPreguntas.size) { nuevoIndice ->
+                            preguntaActual = nuevoIndice
+                            colorBotonVerdadero = Color.Black
+                            colorBotonFalso = Color.Black
+                        }
+                    }
+                ) {
+                    Icon(Icons.Default.CompareArrows, contentDescription = "Icono Flechas Random")
+                    Text(text = "RANDOM")
+                }
+
+                Button(
+                    onClick = {
+                        preguntaActual = (preguntaActual + 1) % listaPreguntas.size
+                        respuestaUsuario = null
+                        /*TODO -> Pasar a la siguiente pregunta*/
+                        if (preguntaActual == 0) {
+                            textoPregunta = listaPreguntas.first().pregunta
+                            imagen = listaPreguntas.first().imagen
+                        }
+                        colorBotonVerdadero = Color.Unspecified
+                        colorBotonFalso = Color.Unspecified
+                    }
+
+                ) {
+                    Text(text = "SIGUIENTE")
+                    Icon(Icons.Default.ArrowForward, contentDescription = "Icono Flecha Siguiente")
+                }
             }
         }
     }
+}
+
+fun seleccionarPreguntaAlAzar(totalPreguntas: Int, onPreguntaSeleccionada: (Int) -> Unit) {
+    val random = Random()
+    val nuevoIndice = random.nextInt(totalPreguntas)
+    onPreguntaSeleccionada(nuevoIndice)
 }
